7526e20d3b13a55cd97927b043e057505eeca0ed
Darciechapman <darciechapman96@gmail.com>
2020-05-01 10:54:03 +0800

    get * returns index.html

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -146,3 +146,6 @@ app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "notes.html"))
 })
 
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"))
+})
